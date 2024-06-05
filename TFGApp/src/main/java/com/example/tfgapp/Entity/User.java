604bc39e5283f4930d3da9b03dc7b2ec08365388
Hugo Avalos <hugoavalos@uma.es>
2024-06-05 13:17:18 +0200

    login

--- TFGApp/src/main/java/com/example/tfgapp/Entity/User.java
+++ TFGApp/src/main/java/com/example/tfgapp/Entity/User.java
@@ -1,6 +1,7 @@
 package com.example.tfgapp.Entity;
 
 import lombok.Data;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.annotation.Id;
 
@@ -11,6 +12,7 @@ public class User {
     @Id
     private String id;
 
+    @Indexed(unique = true) // Asegura que el campo 'username' sea único
     private String username;
     private String password;
 
